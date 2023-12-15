d350a5065f64d8e73a8b4102e2961d99bfc3769b
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-12-15 08:27:55 +0300

    TA#58434 [ADD] rma_groupby_parent_affiliate : new module

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -2,16 +2,14 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Main Module',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Install all addons required for testing.',
-    'depends': [
-        
-    ],
-    'installable': True,
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": ["rma_groupby_parent_affiliate"],
+    "installable": True,
 }
---
+++ rma_groupby_parent_affiliate/README.rst
@@ -0,0 +1,14 @@
+RMA group by Parent Affiliate
+=============================
+This module allows to group by parent affiliate on Return Merchandise Authorization (RMA).
+
+Usage
+-----
+As a user with access to RMA (rma), I see a new “Parent company” column.
+I can also group by parent company.
+
+.. image:: static/description/rma_parent_company.png
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
